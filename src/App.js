aa0c038220813581cd72d0d936339af6c3d57885
N Bhavya Sree <23wh1a0562@bvrithyderabad.edu.in>
2026-04-10 16:13:20 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React, { useEffect } from 'react';
 import { fetchTaskById } from './api/client';
 import './App.css';
 import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import QuestionPage from './pages/QuestionPage';
 
 
 function App() {
-  // useEffect(() => {
-  //   const testApi = async () => {
-  //    const data = await fetchTaskById(1);
-  //    console.log('Fetched task:', data);
-  //   }
-  //   testApi();
-  // }, []);
+  useEffect(() => {
+    const testApi = async () => {
+     const data = await fetchTaskById("1");
+     console.log('Fetched task:', data);
+    }
+    testApi();
+  }, []);
 
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/tasks/:id" element={<QuestionPage />} />
+        <Route path="/task/:id" element={<QuestionPage />} />
       </Routes>
     </Router>
   );
