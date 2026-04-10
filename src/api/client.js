56b73bcebd72ab9eb00876fb3aad7484baff1949
N Bhavya Sree <23wh1a0562@bvrithyderabad.edu.in>
2026-04-10 15:32:54 +0530

    Implemented GET API

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,12 @@
-// TODO: Set the base URL for your API
-
+export const fetchTaskById = async(id) => {
+    try{
+    const response = await fetch(`/api/tasks/${id}`);
+    if (!response.ok) {
+        throw new Error('Task not found');
+    }
+    return await response.json();
+    } catch (error) {
+        console.error('Error fetching task:', error);
+        throw error;
+    }   
+};
